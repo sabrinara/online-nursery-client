7afe67dd5fef839cfdba422e849325e957e16000
TechBuilderTeam <saashid18@gmail.com>
2024-08-13 04:33:55 +0600

    category card design and responsive is done

--- src/components/AllCard/CategoryCard.tsx
+++ src/components/AllCard/CategoryCard.tsx
@@ -57,22 +57,33 @@ const CategoryCard = () => {
     return (
         <div className="my-10 md:my-28">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                {data.map((item , index) => (
-                    <div   className={`rounded-2xl mb-5 ${index % 2 === 0 ? 'translate-y-0 md:translate-y-10' : 'translate-y-0 md:-translate-y-10'}`} 
-                    key={item.id}>
+                {data.slice(0, 7).map((item, index) => (
+                    <div className={`rounded-2xl mb-5 ${index % 2 === 0 ? 'translate-y-0 md:translate-y-10' : 'translate-y-0 md:-translate-y-10'}`}
+                        key={item.id}>
                         <div className="relative group">
                             <img src={item.imageUrl} alt={item.title} className="w-full h-96 " />
                             <div className="absolute inset-0 flex items-center justify-center gap-4 top-3/4 bg-green-200 bg-opacity-20  ">
-                               <div className="flex justify-center items-center gap-1 hover:animate-wobble" title="Show more">
-                               <h1 className="text-xl text-gray-600">{item.category} </h1>
-                               <FaArrowRight className="text-2xl text-gray-600 mt-1"  />
-                               </div>
+                                <div className="flex justify-center items-center gap-1 hover:animate-wobble" title="Show more">
+                                    <h1 className="text-xl text-gray-600">{item.category} </h1>
+                                    <FaArrowRight className="text-2xl text-gray-600 mt-1" />
+                                </div>
                             </div>
                         </div>
-                        {/* <h3 className="text-center mt-4 font-bold text-black text-lg">{item.title}</h3>
-                    <p className="text-center font-bold  text-md  text-green-700">$ {item.price}</p> */}
+
                     </div>
                 ))}
+                <div>
+                    <div className="flex items-center justify-center gap-4 my-20 md:my-0 md:mt-28 hover:animate-wobble">
+                        <button className="relative flex items-center justify-center border-2 border-green-800 text-green-900  px-6 py-3 rounded-ss w-20 h-20 bg-white shadow-lg hover:bg-green-50 transition duration-300">
+
+
+                        </button>
+                        <h1 className="absolute font-semibold text-xl  ml-6 mr-4 bg-white py-1">
+                            View More Categories
+                        </h1>
+                        <FaArrowRight className="  text-green-900 text-lg mt-1 ml-36" />
+                    </div>
+                </div>
             </div>
         </div>
 
