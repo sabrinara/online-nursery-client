ed0d962eda14a17655625d4bd777f0f6d64fa56c
TechBuilderTeam <saashid18@gmail.com>
2024-08-13 23:35:39 +0600

    all product page design done, responsive done

--- src/pages/AllProducts/AllProducts.tsx
+++ src/pages/AllProducts/AllProducts.tsx
@@ -7,17 +7,13 @@ const AllProducts = () => {
         backgroundImage: `url("https://gabtor-store-demo.myshopify.com/cdn/shop/files/collection.jpg?v=1662605082")` ,
      
         }}>
-      <div className="absolute flex flex-col items-center justify-center left-1/3 bottom-10  md:bottom-28   ">
-        <h5 className="font-jost text-xs md:text-lg font-medium md:pb-2 tracking-widest text-green-700">The  Garden</h5>
-        <h2 className="text-xl md:text-6xl font-bold md:mb-2 text-green-100">
-        Deliciosa Plant
+      <div className="absolute flex flex-col items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ">
+       
+        <h2 className="text-3xl md:text-6xl font-bold md:mb-2 text-green-50">
+        Products
         </h2>
-        <p className="text-xs md:text-base font-medium md:mt-4 text-white">
-        It is a long established fact that a reader will be <br/> distracted by the readable content
-        </p>
-        <Button className="border-2  border-white text-white text-xs md:text-base px-[6px] md:px-6 py-0 md:py-6 font-bold rounded-none bg-transparent  mt-2 md:mt-4 hover:bg-green-950 hover:text-white">
-          Learn More
-        </Button>
+        <h5 className="font-jost text-sm md:text-lg font-medium md:pb-2 tracking-widest text-green-100 mt-2">That Helps to heal life.</h5>
+       
       </div>
     </div>
   );
