2282befda98ffff2c34ad3bbfc60e4f01f9b49bc
TechBuilderTeam <saashid18@gmail.com>
2024-08-18 18:02:14 +0600

    payment page done

--- src/pages/CartPayment/Payment.tsx
+++ src/pages/CartPayment/Payment.tsx
@@ -1,16 +1,25 @@
-
+import Lottie from "lottie-react";
+import payment from "../../../public/payment.json";
+import { toast } from "sonner";
 
 const Payment = () => {
+   const handleSubmit = () => {
+        
+        toast.success("Payment Successfull");
+    }
     return (
-        <div>
-           <div>
+        <div className="flex flex-col md:flex-row items-center justify-center ">
+            <div className="w-1/2">
+                <Lottie animationData={payment} loop={true} />
+            </div>
+            <div className="w-1/2 flex flex-col gap-4 mx-10">
+                <h1 className="text-3xl font-bold">Payment: $</h1>
+                <input className="border border-green-900 px-3 py-2 rounded"  type="number" placeholder="Enter the Payment Amount"
 
-           </div>
-           <div>
-            <input className="border border-green-900 px-3 py-2 rounded-none hover:bg-green-900 hover:text-white" type="number" placeholder="Enter Amount"
-            
-            />
-           </div>
+                />
+                <button onClick={handleSubmit} className="border border-green-900 px-3 py-2 rounded bg-green-800 text-white hover:bg-green-950 hover:text-white">
+                    Done</button>
+            </div>
         </div>
     );
 };
