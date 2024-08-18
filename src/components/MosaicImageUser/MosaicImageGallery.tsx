a31535c5266ec577f6d43147f92eef2ab06992ce
TechBuilderTeam <saashid18@gmail.com>
2024-08-18 23:49:19 +0600

    orders api intregated in home and all products page

--- src/components/MosaicImageUser/MosaicImageGallery.tsx
+++ src/components/MosaicImageUser/MosaicImageGallery.tsx
@@ -5,63 +5,15 @@ import { FaArrowRight } from "react-icons/fa";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const MosaicImageGallery = () => {
-    const {data , isLoading} = useGetOrdersQuery({});
+    const { data, isLoading } = useGetOrdersQuery({});
     const [view, setView] = useState(null);
 
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    const { data: orders } = data;
+    console.log(orders);
 
-
-    // const data = [
-    //     {
-    //         id: 1,
-    //         userImage: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
-    //         name: "image 1"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "image 2",
-    //         userImage: "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
-    //     },
-    //     {
-    //         id: 3,
-    //         name: "image 3",
-    //         userImage: "https://images.unsplash.com/photo-1553830591-2f39e38a013c?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
-    //     },
-    //     {
-    //         id: 4,
-    //         name: "image 4",
-    //         userImage: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D"
-    //     },
-    //     {
-    //         id: 5,
-    //         name: "image 5",
-    //         userImage: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAwfHx1c2VyfGVufDB8fDB8fHww"
-    //     },
-    //     {
-    //         id: 6,
-    //         name: "image 6",
-    //         userImage: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
-    //     },
-    //     {
-    //         id: 7,
-    //         name: "image 7",
-    //         userImage: "https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
-    //     },
-    //     {
-    //         id: 8,
-    //         name: "image 8",
-    //         userImage: "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHx1c2VyfGVufDB8fDB8fHww"
-    //     },
-    //     {
-    //         id: 9,
-    //         name: "image 9",
-    //         userImage: "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHx1c2VyfGVufDB8fDB8fHww"
-    //     },
-    //     {
-    //         id: 10,
-    //         name: "image 6",
-    //         userImage: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTZ8fHVzZXJ8ZW58MHx8MHx8fDA%3D"
-    //     }
-    // ];
 
     const viewImage = (img) => {
         setView(img);
@@ -92,7 +44,7 @@ const MosaicImageGallery = () => {
                     columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4, 1500: 5 }}
                 >
                     <Masonry gutter="24px" className="">
-                        {data?.slice(0, 7)?.map((item : TOrders, index : number) => {
+                        {orders?.slice(0, 8)?.map((item: TOrders, index: number) => {
 
                             if (index % 2 === 0) {
                                 return (
@@ -114,10 +66,10 @@ const MosaicImageGallery = () => {
                                                 <p className="text-white text-lg font-bold">{item.name}</p>
                                             </div>
                                         </div>
-                                        {data[index + 1] && (
+                                        {orders[index + 1] && (
                                             <div className="relative">
                                                 <img
-                                                    src={data[index + 1].userImage}
+                                                    src={orders[index + 1].userImage}
                                                     alt=""
                                                     style={{
                                                         width: "100%",
@@ -125,10 +77,10 @@ const MosaicImageGallery = () => {
                                                         height: index % 4 === 0 ? "200px" : "300px",
                                                         objectFit: "cover",
                                                     }}
-                                                    onClick={() => viewImage(data[index + 1].userImage)}
+                                                    onClick={() => viewImage(orders[index + 1].userImage)}
                                                 />
                                                 <div className="absolute bottom-0 left-0 right-0 bg-green-900 bg-opacity-20 p-4 text-center">
-                                                    <p className="text-white text-lg font-bold">{data[index + 1].name}</p>
+                                                    <p className="text-white text-lg font-bold">{orders[index + 1].name}</p>
                                                 </div>
                                             </div>
                                         )}
@@ -144,12 +96,12 @@ const MosaicImageGallery = () => {
             </div>
             <div className="flex items-center justify-center gap-4 mt-6 mb-16 hover:animate-wobble">
                 <button className="relative flex items-center justify-center border-2 border-green-800 text-green-900  px-6 py-3 rounded-full w-20 h-20 bg-white shadow-lg  transition duration-300">
-                    
-                    
+
+
                 </button>
                 <h1 className="absolute font-semibold text-xl  ml-6 bg-white py-1">
-                        View Our More Client's
-                    </h1>
+                    View Our More Client's
+                </h1>
                 <FaArrowRight className="  text-green-900 text-lg mt-1 ml-40" />
             </div>
         </div>
