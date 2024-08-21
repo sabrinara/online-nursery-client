530a8a9092cbc7371c9d5f32b97ea91c74a12e33
TechBuilderTeam <saashid18@gmail.com>
2024-08-21 17:08:27 +0600

    privacy policy page done

--- src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -2,10 +2,55 @@
 
 const PrivacyPolicy = () => {
     return (
-        <div>
-            <h1>Privacy Policy</h1>
+        <div className="md:p-6 mx-4 md:mx-10 text-gray-600">
+          <h1 className="hidden md:flex items-center justify-center mb-8 text-3xl md:text-5xl font-bold my-4 text-center font-serif text-green-900" >NatureNest Privacy Policy</h1>
+          <h1 className="flex md:hidden items-center justify-center mb-4 text-3xl md:text-5xl font-bold my-4 text-center font-serif text-green-900 " >NatureNest <br /> Privacy Policy</h1>
+          <p className="mb-4">
+            At NatureNest, we are committed to protecting your privacy and ensuring your browsing experience is seamless and enjoyable. This Cookie Policy explains how and why we use cookies, the types of cookies we employ, and how you can manage them.
+          </p>
+    
+          <h2 className="text-2xl font-semibold mb-2">What Are Cookies?</h2>
+          <p className="mb-4">
+            Cookies are small text files stored on your device when you visit a website. They contain information that helps websites remember your preferences, enhance your browsing experience, and analyze site performance. While cookies are useful, you can choose whether to accept or decline them.
+          </p>
+    
+          <h2 className="text-2xl font-semibold mb-2">Managing Cookies</h2>
+          <p className="mb-4">
+            Most web browsers accept cookies by default, but you have the option to modify your settings to decline cookies if you prefer. However, please be aware that disabling cookies may impact some functionalities on our website.
+          </p>
+    
+          <h2 className="text-2xl font-semibold mb-2">How We Use Cookies</h2>
+          <p className="mb-4">
+            <strong>NatureNest</strong> uses cookies for various purposes, including:
+          </p>
+          <ul className="list-disc list-inside mb-4">
+            <li className="mb-2">
+              <strong>Essential Cookies:</strong> Necessary for the core functionality of our website, enabling features such as page navigation and secure access to certain areas.
+            </li>
+            <li className="mb-2">
+              <strong>Performance Cookies:</strong> We use Google Analytics to gather anonymous information about how visitors interact with our site. These cookies collect details like the number of visitors, the pages they visit, and their behavior on the site. The data is aggregated and used to improve our website's performance and user experience.
+            </li>
+          </ul>
+    
+          <h2 className="text-2xl font-semibold mb-2">Google Analytics</h2>
+          <p className="mb-4">
+            NatureNest uses Google Analytics to better understand how users interact with our website. Google Analytics uses cookies to collect standard internet log information and visitor behavior data in an anonymous format. The data collected includes your IP address, which is sent to Google and used to evaluate website usage, generate reports, and help us improve the website.
+          </p>
+    
+          <h2 className="text-2xl font-semibold mb-2">Your Privacy</h2>
+          <p className="mb-4">
+            NatureNest and Google do not collect or link any Personally Identifiable Information (PII) through cookies. We will never attempt to identify individual users from IP addresses or other data, and we will not associate any information gathered with any personal details unless explicitly provided by you via forms on our website.
+          </p>
+    
+          <h2 className="text-2xl font-semibold mb-2">Controlling Your Cookies</h2>
+          <p className="mb-4">
+            You have the right to manage how cookies are used. You can adjust your browser settings to accept or reject cookies as needed. If you choose to decline cookies, some features of NatureNest may not function as intended.
+          </p>
+          <p>
+            For more detailed guidance on how to manage or delete cookies, please visit the help pages of your web browser.
+          </p>
         </div>
-    );
+      );
 };
 
 export default PrivacyPolicy;
